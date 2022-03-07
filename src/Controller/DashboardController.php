59ec8a3cb80bee2372eecedc7eb693f8b0b598a5
Ahmed Bougatf <98043267+bougatfa8@users.noreply.github.com>
2022-03-07 22:26:43 +0100

    Add files via upload

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @assert\NotBlank(message="Le Titre est obligatoire hahahaaah")
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 60,
+     *      minMessage = "The Title must be at least {{ limit }} characters long",
+     *      maxMessage = "The Title cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @assert\NotBlank(message="Le Content est obligatoire")
+     * @Assert\Length(
+     *      min = 20,
+     *      max = 250,
+     *      minMessage = "The content must be at least {{ limit }} characters long",
+     *      maxMessage = "The content cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="The Image field is mandatory." , groups={"createHahaha"})
+     * @Assert\File(mimeTypes={ "image/jpeg", "image/png" } , mimeTypesMessage="Please upload a valid Image")
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
